a9774fb3218037004d8f923b480d1764a0ed5d99
Elmin Alirzayev <elmin.alirzayev@gmail.com>
2025-08-12 02:04:17 +0300

    warn

--- DatabaseSink/DatabaseSink.cs
+++ DatabaseSink/DatabaseSink.cs
@@ -29,7 +29,7 @@ namespace Easy.Logging.DatabaseSink_
 #if NETSTANDARD2_1_OR_GREATER ||NETFRAMEWORK
     public class LogDbContext : DbContext
     {
-        public DbSet<LogEntryEntity> Logs { get; set; }
+        public DbSet<LogEntryEntity>? Logs { get; set; }
 
         public LogDbContext() : base("name=LogDbConnection") { }
 
@@ -71,7 +71,7 @@ namespace Easy.Logging.DatabaseSink_
                 Exception = entry.Exception
             };
 
-            _context.Logs.Add(entity);
+            _context.Logs?.Add(entity);
 #if NET6_0_OR_GREATER || NET7_0_OR_GREATER || NET8_0_OR_GREATER || NET9_0_OR_GREATER
             await _context.SaveChangesAsync();
 #endif
